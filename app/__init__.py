22c40a1b9724fa2010cec7eddb5163c2a5871704
Maxim <maxsavikin@gmail.com>
2024-11-15 20:28:02 +0300

    feat: Добавлены материалы по алгебре

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from app.views import HomeView, GeometryView
+from app.views import HomeView, GeometryView, AlgebraView
 
 app = Flask(__name__)
 
 app.add_url_rule('/', view_func=HomeView.as_view("/"))
 app.add_url_rule('/index', view_func=HomeView.as_view("index"))
 app.add_url_rule("/geometry", view_func=GeometryView.as_view("geometry"))
+app.add_url_rule("/algebra", view_func=AlgebraView.as_view("algebra"))
--- app/views.py
+++ app/views.py
@@ -22,3 +22,18 @@ class GeometryView(MethodView):
             11: "/static/geom11.pdf"
         }
         return render_template("geom.html", file_path=geom_dict[class_number], class_number=class_number)
+
+
+class AlgebraView(MethodView):
+    """Класс представления страницы 'Алгебра' """
+
+    def get(self) -> str:
+        class_number = request.args.get('class', default=7, type=int)
+        algebra_dict = {
+            7: "/static/algebra7.pdf",
+            8: "/static/algebra8.pdf",
+            9: "/static/algebra9.pdf",
+            10: "/static/algebra10.pdf",
+            11: "/static/algebra11.pdf"
+        }
+        return render_template("algebra.html", file_path=algebra_dict[class_number], class_number=class_number)
